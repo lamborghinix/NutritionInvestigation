76dfabac9e490e7e17922e8218c05f08c4608460
Lambo Qian <lamborghinister@gmail.com>
2016-10-14 00:20:50 +0800

    Add DB ORM

--- NutritionInvestigation/NutritionInvestigation/Form1.cs
+++ NutritionInvestigation/NutritionInvestigation/Form1.cs
@@ -16,5 +16,20 @@ namespace NutritionInvestigation
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myDBEntities context = new myDBEntities();
+            FoodClass newFoodClass = new FoodClass();
+            newFoodClass.FoodClassName = "粮谷类";
+            newFoodClass.FoodIndex = "F01";
+            newFoodClass.ParentID = 0;
+            newFoodClass.ParentName = string.Empty;
+
+            context.FoodClasses.Add(newFoodClass);
+            context.SaveChanges();
+
+            MessageBox.Show("add data OK.");
+        }
     }
 }
